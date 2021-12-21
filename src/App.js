b2ed69a6089d0e3ef532d0b0d621c7148817005e
Carlos <capz2003@gmail.com>
2021-12-21 07:54:47 -0500

    Sets App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom'
+
+function App () {
+  return (
+    <div>hello</div>
+  )
+}
+
+
+export default App
