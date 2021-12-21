113566d93e6d0a1b2c4ca9f44b34cf42d8c5efe9
Carlos <capz2003@gmail.com>
2021-12-21 08:11:48 -0500

    Sets component files for project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import react from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import styled from "styled-components";
 
 function App () {
   return (
     <div>hello</div>
   )
 }
+
+
 
 
 export default App
